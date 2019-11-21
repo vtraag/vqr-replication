7aa06a3b1a5963a93f2d9dfdd05ab53db77822ec
vtraag <vincent@traag.net>
2019-11-21 17:30:14 +0100

    Changed to work with MAD and MAPD.

--- analysis_WoS.py
+++ analysis_WoS.py
@@ -31,39 +31,45 @@ gev_names_df = pd.DataFrame(
   ['13', "Economics and Statistics"]], columns=['GEV_id', 'GEV'])
 
 #%%
-sql = """  
-    SELECT *
-      FROM VQR
-      INNER JOIN VQR_WoS_indic AS indic
-        ON indic.ut = VQR.ut
-  """;        
-df = pd.io.sql.read_sql(sql, get_conn());
-
-# Randomly pick either reviewer 1 or reviewer 2 as reviewer 1.
-np.random.seed(0)
-df['reviewer'] = np.random.randint(1, 3, size=df.shape[0])
-rev_1_cols = [c for c in df.columns if 'REV_1' in c]
-rev_2_cols = [c for c in df.columns if 'REV_2' in c]
-df[rev_1_cols + rev_2_cols] = df[rev_1_cols + rev_2_cols].where(
-                                df['reviewer'] == 1, 
-                                df[rev_1_cols + rev_2_cols]);
-
-# Limit to universities
-df = df[df['INSTITUTION_TYPE'] == 'U']
-
-# Limit to only WoS publications
-#df = df[df['USED_DB'] == 'WoS']
-
-df['GEV_numeric'] = df['GEV'].apply(lambda x: int(re.match(r'\d*', x)[0]))
-df = df.set_index(['INSTITUTION_ID', 'GEV']);
-
-df = df.sort_values('GEV_numeric')
-
-df['REV_1_SCORE'] = df[['REV_1_ORIGINALITY', 'REV_1_RIGOR', 'REV_1_IMPACT']].sum(axis=1)
-df['REV_2_SCORE'] = df[['REV_2_ORIGINALITY', 'REV_2_RIGOR', 'REV_2_IMPACT']].sum(axis=1)
-df['REV_SCORE'] = df[['REV_1_SCORE', 'REV_2_SCORE']].mean(axis=1)
-
-df.to_csv('../data/WoS_indic.csv')
+try:
+  conn = get_conn()
+except:
+  conn = None
+    
+if conn:
+  sql = """  
+      SELECT *
+        FROM VQR
+        INNER JOIN VQR_WoS_indic AS indic
+          ON indic.ut = VQR.ut
+    """;        
+  df = pd.io.sql.read_sql(sql, conn);
+  conn.close()
+  # Randomly pick either reviewer 1 or reviewer 2 as reviewer 1.
+  np.random.seed(0)
+  df['reviewer'] = np.random.randint(1, 3, size=df.shape[0])
+  rev_1_cols = [c for c in df.columns if 'REV_1' in c]
+  rev_2_cols = [c for c in df.columns if 'REV_2' in c]
+  df[rev_1_cols + rev_2_cols] = df[rev_1_cols + rev_2_cols].where(
+                                  df['reviewer'] == 1, 
+                                  df[rev_1_cols + rev_2_cols]);
+  
+  # Limit to universities
+  df = df[df['INSTITUTION_TYPE'] == 'U']
+  
+  # Limit to only WoS publications
+  #df = df[df['USED_DB'] == 'WoS']
+  
+  df['GEV_numeric'] = df['GEV'].apply(lambda x: int(re.match(r'\d*', x)[0]))
+  df = df.set_index(['INSTITUTION_ID', 'GEV']);
+  
+  df = df.sort_values('GEV_numeric')
+  
+  df['REV_1_SCORE'] = df[['REV_1_ORIGINALITY', 'REV_1_RIGOR', 'REV_1_IMPACT']].sum(axis=1)
+  df['REV_2_SCORE'] = df[['REV_2_ORIGINALITY', 'REV_2_RIGOR', 'REV_2_IMPACT']].sum(axis=1)
+  df['REV_SCORE'] = df[['REV_1_SCORE', 'REV_2_SCORE']].mean(axis=1)
+  
+  df.to_csv('../data/WoS_indic.csv')
 
 #%%
 
@@ -81,8 +87,6 @@ corr_individual = df[cols].corr('spearman')
 #%% Calculate outcome results on institutional level  (overall)
 inst_df = df[cols].groupby(level=['INSTITUTION_ID', 'GEV'], sort=False).mean()
 
-#%% Sample size per GEV
-n = df.groupby(level=['GEV'], sort=False).size()
 #%% #%% Sample size per GEV per institutions
 n_per_institution = df.groupby(level=['INSTITUTION_ID', 'GEV'], sort=False).size()
 n_per_institution.name = 'n_pubs'
@@ -181,7 +185,6 @@ def calc_MAD_and_MAPD_inst(df):
     if GEV != '13':
       res_perc_cit = smf.ols(formula='REV_1_SCORE ~ PERCENTILE_CITATIONS', data=GEV_df).fit()
       res_perc_ind = smf.ols(formula='REV_1_SCORE ~ PERCENTILE_INDICATOR_VALUE', data=GEV_df).fit()
-    res_rev_2 = smf.ols(formula='REV_1_SCORE ~ REV_2_SCORE', data=GEV_df).fit()
     
     GEV_df['pred_ncs'] = res_ncs.predict()
     GEV_df['pred_njs'] = res_njs.predict()
@@ -191,20 +194,19 @@ def calc_MAD_and_MAPD_inst(df):
     else:
       GEV_df['pred_perc_cit'] = np.nan
       GEV_df['pred_perc_ind'] = np.nan
-    GEV_df['pred_rev_2'] = res_rev_2.predict()
     
     GEV_df['abs_diff_ncs'] = np.abs(GEV_df['pred_ncs'] - GEV_df['REV_1_SCORE'])
     GEV_df['abs_diff_njs'] = np.abs(GEV_df['pred_njs'] - GEV_df['REV_1_SCORE'])
     GEV_df['abs_diff_perc_cit'] = np.abs(GEV_df['pred_perc_cit'] - GEV_df['REV_1_SCORE'])
     GEV_df['abs_diff_perc_ind'] = np.abs(GEV_df['pred_perc_ind'] - GEV_df['REV_1_SCORE'])
-    GEV_df['abs_diff_rev_2'] = np.abs(GEV_df['pred_rev_2'] - GEV_df['REV_1_SCORE'])
+    GEV_df['abs_diff_rev_2'] = np.abs(GEV_df['REV_2_SCORE'] - GEV_df['REV_1_SCORE'])
   
     MAD = GEV_df.median()[['abs_diff_ncs', 'abs_diff_njs', 'abs_diff_perc_cit', 'abs_diff_perc_ind', 'abs_diff_rev_2']]
     MAD['GEV'] = GEV
     MADs.append(MAD)
 
     cols = ['REV_1_SCORE', 'REV_2_SCORE', 'ncs', 'njs',
-            'pred_ncs', 'pred_njs', 'pred_perc_cit', 'pred_perc_ind', 'pred_rev_2']
+            'pred_ncs', 'pred_njs', 'pred_perc_cit', 'pred_perc_ind']
     
     GEV_df = GEV_df[cols + ['n_pubs']].copy()
     GEV_df[cols] = GEV_df[cols].mul(GEV_df['n_pubs'], axis=0)
@@ -218,7 +220,7 @@ def calc_MAD_and_MAPD_inst(df):
     GEV_df['abs_perc_diff_njs'] = abs_perc_diff(GEV_df['pred_njs'], GEV_df['REV_1_SCORE'])
     GEV_df['abs_perc_diff_perc_cit'] = abs_perc_diff(GEV_df['pred_perc_cit'], GEV_df['REV_1_SCORE'])
     GEV_df['abs_perc_diff_perc_ind'] = abs_perc_diff(GEV_df['pred_perc_ind'], GEV_df['REV_1_SCORE'])
-    GEV_df['abs_perc_diff_rev_2'] = abs_perc_diff(GEV_df['pred_rev_2'], GEV_df['REV_1_SCORE'])
+    GEV_df['abs_perc_diff_rev_2'] = abs_perc_diff(GEV_df['REV_2_SCORE'], GEV_df['REV_1_SCORE'])
     
     MAPD = 100*GEV_df.median()[['abs_perc_diff_ncs', 'abs_perc_diff_njs', 'abs_perc_diff_perc_cit', 'abs_perc_diff_perc_ind', 'abs_perc_diff_rev_2']]
     MAPD['GEV'] = GEV
@@ -234,7 +236,8 @@ def bootstrap_MAD_MAPD_inst(df, N):
     bootstrap_df = df.iloc[np.random.randint(n, size=n)]  
     yield(calc_MAD_and_MAPD_inst(bootstrap_df))
 #%%
-limited_df = pd.merge(df.reset_index(), n_per_institution[n_per_institution >= 1].reset_index(), on=['INSTITUTION_ID', 'GEV'])
+min_n_per_institution = 1
+limited_df = pd.merge(df.reset_index(), n_per_institution[n_per_institution >= min_n_per_institution].reset_index(), on=['INSTITUTION_ID', 'GEV'])
 #%%
 MAD_MAPD_bootstrap = list(bootstrap_MAD_MAPD_inst(limited_df, N=1000))
 #%%
@@ -243,9 +246,9 @@ MAD_bootstrap, MAPD_bootstrap = zip(*MAD_MAPD_bootstrap)
 MAD, MAPD = calc_MAD_and_MAPD_inst(limited_df)
 #%%
 conf_interval = 0.95
-low_bound = pd.concat(MAD_bootstrap).groupby('GEV').aggregate([lambda x: np.percentile(x, 1 - conf_interval/2)])
+low_bound = pd.concat(MAD_bootstrap).groupby('GEV').aggregate([lambda x: np.percentile(x, 100*(1 - conf_interval)/2)])
 low_bound.columns.set_levels(['lb'], level=1, inplace=True)
-up_bound = pd.concat(MAD_bootstrap).groupby('GEV').aggregate([lambda x: np.percentile(x, (1 - conf_interval/2) + conf_interval)])
+up_bound = pd.concat(MAD_bootstrap).groupby('GEV').aggregate([lambda x: np.percentile(x, 100*((1 - conf_interval)/2 + conf_interval))])
 up_bound.columns.set_levels(['ub'], level=1, inplace=True)
 MAD.columns = pd.MultiIndex.from_tuples([(c, 'empirical') for c in MAD.columns])
 MAD = pd.concat([MAD, low_bound, up_bound], axis=1)
@@ -253,9 +256,9 @@ MAD = MAD.sort_index(axis=1)
 MAD = pd.merge(gev_names_df, MAD, left_on='GEV_id', right_index=True)
 #%%
 
-low_bound = pd.concat(MAPD_bootstrap).groupby('GEV').aggregate([lambda x: np.percentile(x, 1 - conf_interval/2)])
+low_bound = pd.concat(MAPD_bootstrap).groupby('GEV').aggregate([lambda x: np.percentile(x, 100*(1 - conf_interval)/2)])
 low_bound.columns.set_levels(['lb'], level=1, inplace=True)
-up_bound = pd.concat(MAPD_bootstrap).groupby('GEV').aggregate([lambda x: np.percentile(x, (1 - conf_interval/2) + conf_interval)])
+up_bound = pd.concat(MAPD_bootstrap).groupby('GEV').aggregate([lambda x: np.percentile(x, 100*((1 - conf_interval)/2 + conf_interval))])
 up_bound.columns.set_levels(['ub'], level=1, inplace=True)
 MAPD.columns = pd.MultiIndex.from_tuples([(c, 'empirical') for c in MAPD.columns])
 MAPD = pd.concat([MAPD, low_bound, up_bound], axis=1)
@@ -263,8 +266,8 @@ MAPD = MAPD.sort_index(axis=1)
 MAPD = pd.merge(gev_names_df, MAPD, left_on='GEV_id', right_index=True)
 #%%
 
-MAD.to_csv('../results/MAD.csv')
-MAPD.to_csv('../results/MAPD.csv')
+MAD.to_csv('../results/MAD_min={0}.csv'.format(min_n_per_institution))
+MAPD.to_csv('../results/MAPD_min={0}.csv'.format(min_n_per_institution))
 
 #%%
 
@@ -300,8 +303,8 @@ plt.legend(bars, list(zip(*ycols))[1],
            ncol=5)
 plt.grid(axis='y', ls=':')
 sns.despine()
-plt.savefig('../results/MAD.pdf', bbox_inches='tight')
-plt.savefig('../results/MAD.png', dpi=300, bbox_inches='tight')
+plt.savefig('../results/MAD_min={0}.pdf'.format(min_n_per_institution), bbox_inches='tight')
+plt.savefig('../results/MAD_min={0}.png'.format(min_n_per_institution), dpi=300, bbox_inches='tight')
 
 #%%
 
@@ -337,8 +340,8 @@ plt.legend(bars, list(zip(*ycols))[1],
            ncol=5)
 plt.grid(axis='y', ls=':')
 sns.despine()
-plt.savefig('../results/MAPD.pdf', bbox_inches='tight')
-plt.savefig('../results/MAPD.png', dpi=300, bbox_inches='tight')
+plt.savefig('../results/MAPD_min={0}.pdf'.format(min_n_per_institution), bbox_inches='tight')
+plt.savefig('../results/MAPD_min={0}.png'.format(min_n_per_institution), dpi=300, bbox_inches='tight')
 #%% Calculate MAD on individual level
 def calc_MAD_ind(df):
   
@@ -351,7 +354,6 @@ def calc_MAD_ind(df):
     if GEV != '13':
       res_perc_cit = smf.ols(formula='REV_1_SCORE ~ PERCENTILE_CITATIONS', data=GEV_df).fit()
       res_perc_ind = smf.ols(formula='REV_1_SCORE ~ PERCENTILE_INDICATOR_VALUE', data=GEV_df).fit()
-    res_rev_2 = smf.ols(formula='REV_1_SCORE ~ REV_2_SCORE', data=GEV_df).fit()
     
     GEV_df['pred_ncs'] = res_ncs.predict()
     GEV_df['pred_njs'] = res_njs.predict()
@@ -361,13 +363,12 @@ def calc_MAD_ind(df):
     else:
       GEV_df['pred_perc_cit'] = np.nan
       GEV_df['pred_perc_ind'] = np.nan
-    GEV_df['pred_rev_2'] = res_rev_2.predict()
     
     GEV_df['abs_diff_ncs'] = np.abs(GEV_df['pred_ncs'] - GEV_df['REV_1_SCORE'])
     GEV_df['abs_diff_njs'] = np.abs(GEV_df['pred_njs'] - GEV_df['REV_1_SCORE'])
     GEV_df['abs_diff_perc_cit'] = np.abs(GEV_df['pred_perc_cit'] - GEV_df['REV_1_SCORE'])
     GEV_df['abs_diff_perc_ind'] = np.abs(GEV_df['pred_perc_ind'] - GEV_df['REV_1_SCORE'])
-    GEV_df['abs_diff_rev_2'] = np.abs(GEV_df['pred_rev_2'] - GEV_df['REV_1_SCORE'])
+    GEV_df['abs_diff_rev_2'] = np.abs(GEV_df['REV_2_SCORE'] - GEV_df['REV_1_SCORE'])
   
     MAD = GEV_df.median()[['abs_diff_ncs', 'abs_diff_njs', 'abs_diff_perc_cit', 'abs_diff_perc_ind', 'abs_diff_rev_2']]
     MAD['GEV'] = GEV
