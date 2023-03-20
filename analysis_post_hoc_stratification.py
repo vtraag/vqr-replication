ea3f520f451bed7eb227ccdd79d5b68fa85690fc
Vincent Traag <vincent@traag.net>
2023-03-20 23:11:03 +0100

    Updated post-hoc stratification figure using stratified sampling for theoretical expectation.

--- analysis_post_hoc_stratification.py
+++ analysis_post_hoc_stratification.py
@@ -15,30 +15,44 @@ if not os.path.exists(figure_dir):
 #%% Read institutional data
 stratification_df = pd.read_csv('../data/public/institutional.csv', index_col='INSTITUTION_ID')
 
-stratification_df['prop_pubs_in_study'] = stratification_df['in_sample'].fillna(0)/stratification_df['VQR_submissions']
+inst_df = stratification_df.groupby('INSTITUTION_ID').sum()
+inst_df['prop_pubs_in_study'] = inst_df['in_sample'].fillna(0)/inst_df['VQR_submissions']
 
 #%%
-all_pubs = np.repeat(stratification_df.index, stratification_df['VQR_submissions'])
 
 n_samples = 1000
-sample_size = (int)(stratification_df['in_sample'].sum())
 n_pubs_samples = []
-for i in range(n_samples):
-  sample = pd.Series(np.random.choice(all_pubs, sample_size, replace=False))
-  n_pubs_sample = sample.groupby(sample).size()
-  n_pubs_samples.append(n_pubs_sample)
 
+for GEV, GEV_df in stratification_df.groupby('GEV_id'):
+  all_pubs = np.repeat(GEV_df.index, GEV_df['VQR_submissions'])
+
+  sample_size = (int)(GEV_df['in_sample'].sum())
+  for i in range(n_samples):
+    sample = pd.Series(np.random.choice(all_pubs, sample_size, replace=False))
+    n_pubs_sample = sample.groupby(sample).size()
+    n_pubs_sample.index.name = 'INSTITUTION_ID'
+    n_pubs_sample = (pd.DataFrame(n_pubs_sample)
+                     .assign(GEV_id=GEV)
+                     .set_index('GEV_id', append=True)
+                    )
+    n_pubs_sample.columns = [i]
+    n_pubs_samples.append(n_pubs_sample)
+
+#%%
 sample_df = (pd.concat(n_pubs_samples, axis=1)
+             .melt(var_name='sample',ignore_index=False)
+             .groupby(['INSTITUTION_ID', 'sample'])
+             .sum()
+             .unstack('sample')
              .fillna(0)
-             .sort_index()
-             .div(stratification_df['VQR_submissions'], axis='index')
+             .div(inst_df['VQR_submissions'], axis='index')
             )
 
 #%% Plot histogram
 
 plt.figure(figsize=(4, 3))
 bins = np.linspace(0, 15/100, 16)
-sns.histplot(stratification_df, x='prop_pubs_in_study',
+sns.histplot(inst_df, x='prop_pubs_in_study',
              bins=bins, element='step', alpha=0.4,
              label='Empirical sample')
 sns.kdeplot(data=sample_df.melt(), x='value', label='Theoretical expectation')
@@ -46,7 +60,7 @@ plt.grid(axis='y', ls=':')
 plt.gca().xaxis.set_major_formatter(mtick.PercentFormatter(xmax=1))
 plt.ylabel('Frequency')
 plt.xlabel('Sample %')
-plt.legend(loc='lower center', bbox_to_anchor=(0.5, 1), 
+plt.legend(loc='lower center', bbox_to_anchor=(0.5, 1),
            ncol=2,
            frameon=False)
 plt.xlim(0, 0.15)
